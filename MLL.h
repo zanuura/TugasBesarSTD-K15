88cc7876b9ac106907a2686f9992e8575bf6d4c0
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2025-12-15 18:41:47 +0700

    outline untuk semua fungsionalitas utama dari spesifikasi (11 prosedur/fungsi utama)

--- MLL.h
+++ MLL.h
@@ -72,4 +72,42 @@ void handleInsertParent(list_orangTua &L);
 void insertFirstParent(list_orangTua &L, adr_orangTua P);
 adr_orangTua createNewParent(infotype_orangTua info); // Fungsi pembantu untuk membuat node baru
 
+// 2. Show all data parent
+void showAllParent(list_orangTua L);
+
+// 3. Menghapus data parent beserta relasinya
+// menggunakan deleteLast untuk list parent
+bool deleteParent(list_orangTua &L_Ortu, list_anak &L_Anak, string namaParent, adr_orangTua &P_Hapus);
+void deleteLastParent(list_orangTua &L, adr_orangTua &P_Hapus);
+// mengunakan deleteFirst untuk list anak
+void deleteFirstChild(list_anak &L, adr_anak &C_Hapus);
+
+// 4. Mencari data parent
+adr_orangTua findParent(list_orangTua L, string namaParent);
+
+// 5. Mencari data child
+adr_anak findChild(list_anak L, string namaChild);
+
+// 6. Menambahkan data child dari belakang
+void insertLastChild(list_anak &L, adr_anak C);
+adr_anak createNewChild(infotype_anak info); // Fungsi pembantu untuk membuat node baru
+
+// 7. Menghubungkan data parent ke data child
+bool hubungkanParentToChild(list_orangTua &L_Ortu, adr_anak C, string namaAyah, string namaIbu);
+adr_relation createNewRelation(adr_anak C); // C adalah pointer ke anak
+
+// 8. Menampilkan seluruh data parent beserta childnya
+void showAllParentAndChild(list_orangTua L_Ortu);
+
+// 9. Mencari data child pada parent tertentu
+adr_anak findChildOnParent(list_orangTua L_Ortu, string namaParent, string namaChild);
+
+// 10. Menghapus data child pada parent tertentu beserta relasinya
+bool deleteRelation(list_orangTua &L_Ortu, string namaParent, string namaChild);
+// Tambahan: Prosedur untuk menghapus Node Anak di L2 jika semua relasinya sudah dihapus
+// adr_anak deleteChildNode(list_anak &L_Anak, string namaChild);
+
+// 11. Menghitung jumlah data child dari parent tertentu
+int countChildOfParent(list_orangTua L, string namaParent);
+
 #endif // MLL_H_INCLUDED
